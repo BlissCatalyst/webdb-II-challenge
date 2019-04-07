bdae60921682c0248e0606ac232621a77aa0207a
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-07 15:52:37 -0600

    can post a new name of a zoo.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +11,34 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// ********** POST **********
+server.post("/api/zoos", async (req, res) => {
+  const zooName = req.body;
+  try {
+    if (zooName && zooName.name) {
+      const [id] = await db("zoos").insert(zooName);
+
+      const newId = await db("zoos")
+        .select("id", "name")
+        .where({ id })
+        .first();
+
+      res.status(201).json({
+        message: "You've just created a new zoo!",
+        id: newId.id,
+        name: newId.name
+      });
+    } else {
+      res
+        .status(400)
+        .json({ message: "You must include a name for the new zoo!" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The server is incabable of making a new zoo right now."
+    });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
